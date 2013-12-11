f156c5c21ad09f8fbd679cd0583b07b70e95e21d
pvenkatakrishnan <pvenkatakrishnan@paypal.com>
2013-12-10 17:39:15 -0800

    Fixing the views tests as well to verify the code

--- test/fixtures/controllers/controller.js
+++ test/fixtures/controllers/controller.js
@@ -33,4 +33,19 @@ module.exports = function (app) {
         throw new Error('uh oh');
     });
 
+    app.on('event:serverError', function (req, res, err) {
+        var data = {};
+        data.url = req.url;
+        data.err = err;
+        res.status(500);
+        res.render('errors/500', data);
+    });
+
+    app.on('event:fileNotFound', function (req, res) {
+        var data = {};
+        data.url = req.url;
+        res.status(404);
+        res.render('errors/404', data);
+    });
+
 };
