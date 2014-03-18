f19bfc37bf6d1beef6f0deb06342a8044693ad03
Jeff Harrell <jeff@juxtadesign.com>
2014-03-17 17:53:37 -0700

    Adding 404 and 500 error middleware

---
+++ middleware/500.js
@@ -0,0 +1,34 @@
+/*───────────────────────────────────────────────────────────────────────────*\
+ │  Copyright (C) 2014 eBay Software Foundation                                │
+ │                                                                             │
+ │hh ,'""`.                                                                    │
+ │  / _  _ \  Licensed under the Apache License, Version 2.0 (the "License");  │
+ │  |(@)(@)|  you may not use this file except in compliance with the License. │
+ │  )  __  (  You may obtain a copy of the License at                          │
+ │ /,'))((`.\                                                                  │
+ │(( ((  )) ))    http://www.apache.org/licenses/LICENSE-2.0                   │
+ │ `\ `)(' /'                                                                  │
+ │                                                                             │
+ │   Unless required by applicable law or agreed to in writing, software       │
+ │   distributed under the License is distributed on an "AS IS" BASIS,         │
+ │   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  │
+ │   See the License for the specific language governing permissions and       │
+ │   limitations under the License.                                            │
+ \*───────────────────────────────────────────────────────────────────────────*/
+'use strict';
+
+
+module.exports = function (template) {
+
+    return function serverError(err, req, res, next) {
+        var model = { url: req.url, err: err };
+
+        if (req.xhr) {
+            res.send(500, model);
+        } else {
+            res.status(500);
+            res.render(template, model);
+        }
+    };
+    
+};
