4632048485fb5d6853a7d74cbe6f6c99a540b193
ertoth <ertoth@paypal.com>
2014-04-02 07:10:49 -0700

    Checkpoint checkin of config changes.

---
+++ api.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var factory = confit(options);
+factory
+    .override('path.json')
+    .override('path.json');
+    .override('path.json');
+    .override('path.json');
+    .create(function (err, config) {
+        // config.get;
+        // config.set;
+        // config.use;
+    });
